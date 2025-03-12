1d2a656a3c836f81a967ddef787efd68832e51bf
Keenan Bromley <bromleyk@oregonstate.edu>
2025-03-11 23:06:44 -0700

    Added tests to water_dispenser_spec

--- spec/water_dispenser_spec.rb
+++ spec/water_dispenser_spec.rb
@@ -1,5 +1,10 @@
 require_relative '../lib/water_dispenser'
 
 describe 'A water dispenser' do
+  it 'sets the reservoir' do
+    reservoir = WaterReservoir.new
+    dispenser = WaterDispenser.new(reservoir)
 
+    expect(dispenser.reservoir).to eq(reservoir)
+  end
 end
