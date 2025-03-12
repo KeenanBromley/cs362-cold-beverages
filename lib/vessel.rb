023f980f731c80ab672acec043d74d9487c10044
Keenan Bromley <bromleyk@oregonstate.edu>
2025-03-11 22:48:58 -0700

    Finished step 1

--- lib/vessel.rb
+++ lib/vessel.rb
@@ -1,9 +1,26 @@
 class Vessel
   attr_reader :volume
 
-  def initialize(name, volume)
+  def initialize(name = 'FAKE', volume = 100)
     @name = name
     @volume = volume
+    @empty = true
+  end
+
+  def name
+    @name
+  end
+
+  def volume
+    @volume
+  end
+
+  def fill
+    @empty = false
+  end
+
+  def empty?
+    @empty
   end
 
 end
