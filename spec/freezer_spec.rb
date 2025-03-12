29057141ece8b05136d1b15e4712235cee5eec3f
Keenan Bromley <bromleyk@oregonstate.edu>
2025-03-12 11:46:23 -0700

    Added tests to freezer_spec

--- spec/freezer_spec.rb
+++ spec/freezer_spec.rb
@@ -1,5 +1,33 @@
 require_relative '../lib/freezer'
 
 describe 'A freezer' do
+  it 'initializes without paramters' do
+    freezer = Freezer.new
+
+    expect(freezer.capacity).to eq(100)
+    expect(freezer.temperature).to eq(70)
+  end
+
+  it 'initializes with paremeters' do
+    freezer = Freezer.new(150)
+
+    expect(freezer.capacity).to eq(150)
+    expect(freezer.temperature).to eq(70)
+  end
+
+  it 'adds item and updates capacity' do
+    freezer = Freezer.new
+    item = Item.new('Fake Item', 50)
+
+    freezer.add(item)
+    expect(freezer.remaining_capacity).to eq(50)
+  end
+
+  it 'sets freezer level and updates temp' do
+    freezer = Freezer.new
+
+    freezer.set_level(5)
+    expect(freezer.temperature).to eq(20)
+  end
 
 end
