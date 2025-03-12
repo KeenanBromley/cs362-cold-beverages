38a59a18bf82560ceefb42413e2ba72d902b93f8
Keenan Bromley <bromleyk@oregonstate.edu>
2025-03-11 23:01:32 -0700

    Added tests to item_spec

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,5 +1,10 @@
 require_relative '../lib/item'
 
 describe 'An item of food or a beverage' do
+  it 'has a name and volume' do
+    item = Item.new('Test Item', 10)
+    expect(item.name).to eq('Test Item')
+    expect(item.volume).to eq(10)
+  end
 
 end
